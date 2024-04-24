aadc290d0d00627ef6a42b3ab6d3583ae2813dcd
Sebastjan Prachovskij <sebastjan.prachovskij@gmail.com>
2024-04-24 23:30:46 +0300

    Add comparison page

---
+++ app/controllers/comparisons_controller.rb
@@ -0,0 +1,11 @@
+class ComparisonsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @product_id = params[:product_id]
+    start_date = params[:start_date].presence ? Date.parse(params[:start_date]) : (Date.today - 30.days)
+    end_date = params[:end_date].presence ? Date.parse(params[:end_date]) : Date.today
+
+    @comparisons = SearchResult.where(product_id: @product_id).where(date: start_date..end_date).select('date, gl, AVG(extracted_total_price) as average_price').group('date, gl').order('date DESC, gl')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
-require 'sidekiq/web'
+require "sidekiq/web"
 
 Rails.application.routes.draw do
-  get 'search/index'
-  post 'search', to: 'search#index'
-  get 'search_history', to: 'home#search_history'
-  get '/privacy', to: 'home#privacy'
-  get '/terms', to: 'home#terms'
-  get 'product_graph/:id', to: 'home#product_graph', as: :product_graph
-  get 'product_details/:product_id/:gl', to: 'home#product_details', as: :product_details
+  get "search/index"
+  post "search", to: "search#index"
+  get "search_history", to: "home#search_history"
+  get "/privacy", to: "home#privacy"
+  get "/terms", to: "home#terms"
+  get "product_graph/:id", to: "home#product_graph", as: :product_graph
+  get "product_details/:product_id/:gl", to: "home#product_details", as: :product_details
+  get "comparisons/:product_id", to: "comparisons#show", as: "comparison"
+  
 authenticate :user, lambda { |u| u.admin? } do
-  mount Sidekiq::Web => '/sidekiq'
+  mount Sidekiq::Web => "/sidekiq"
 
   namespace :madmin do
     resources :impersonates do
@@ -22,7 +24,7 @@ end
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
+  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
